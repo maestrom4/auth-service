46da9da393d6b6415e469e1699c105ac2874cf1c
Danilo Matias <maestro.m4@gmail.com>
2024-05-01 14:59:05 +0800

    edit: move GraphQl handler to modules

--- internal/routes/modules.go
+++ internal/routes/modules.go
@@ -2,10 +2,14 @@ package routes
 
 import (
 	cfg "auth-service/internal/config"
+	v "auth-service/internal/config"
+	"auth-service/internal/graphql"
 	"auth-service/pkg/mongodb"
+	"context"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/graphql-go/handler"
 )
 
 func VerifyEmail() gin.HandlerFunc {
@@ -21,4 +25,22 @@ func VerifyEmail() gin.HandlerFunc {
 		c.JSON(http.StatusOK, gin.H{"isSuccess": true, "message": "Email verified successfully!"})
 	}
 
+}
+
+func GraphQLHandler() gin.HandlerFunc {
+	h := handler.New(&handler.Config{
+		Schema:   &graphql.Schema,
+		Pretty:   true,
+		GraphiQL: true,
+	})
+
+	return func(c *gin.Context) {
+		resolver := c.MustGet(string(v.ResolverKey))
+		userID := c.MustGet(string(v.UserIDKey))
+		ctx := c.Request.Context()
+		ctx = context.WithValue(ctx, string(v.ResolverKey), resolver)
+		ctx = context.WithValue(ctx, string(v.UserIDKey), userID)
+		rWithCtx := c.Request.WithContext(ctx)
+		h.ServeHTTP(c.Writer, rWithCtx)
+	}
 }
